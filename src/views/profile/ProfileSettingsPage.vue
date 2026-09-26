7337e79b80445c3f690e3e728e1f1b68a1736b39
DSF100 <deansingfat@gmail.com>
2026-09-26 15:49:51 +0800

    linked the function

--- src/views/profile/ProfileSettingsPage.vue
+++ src/views/profile/ProfileSettingsPage.vue
@@ -64,6 +64,7 @@ import LanguageSwitcher from '../../components/LanguageSwitcher.vue';
 import ThemeCustomizerModal from '../../components/ThemeCustomizerModal.vue';
 import { clearAllData } from '../../db/database';
 import { getNavMode, setNavMode } from '../../config/app';
+import { forceCancelAllReminders } from '../../services/notifications.js';
 
 const { t } = useI18n();
 
@@ -82,6 +83,7 @@ async function confirmReset(): Promise<void> {
           void clearAllData().then(() => {
             try {
               localStorage.clear();
+              forceCancelAllReminders;
             } catch (e) {
               console.error('reset failed', e);
             }
